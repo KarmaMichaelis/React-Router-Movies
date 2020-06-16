0af2b397f6bb68876e78d67132cf376aae47d767
KamaMichaelis <achantayd@gmail.com>
2020-06-15 23:08:20 -0400

    MovieList added in router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {Route} from 'react-router-dom'
 import SavedList from './Movies/SavedList';
+//import Movie from './Movies/Movie'
+import MovieList from './Movies/MovieList';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -28,7 +30,9 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route path='/movies' component={MovieList}/>
+        
+        
     </div>
   );
 };
